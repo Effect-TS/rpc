378b7070ab4b14068edafcac74a5101932a07d81
Tim Smart <hello@timsmart.co>
2023-03-27 20:21:45 +1300

    chore: update basic example

--- examples/basic.ts
+++ examples/basic.ts
@@ -1,3 +1,5 @@
+import { pipe } from "@effect/data/Function"
+import * as Chunk from "@effect/data/Chunk"
 import * as Effect from "@effect/io/Effect"
 import * as Client from "@effect/rpc/Client"
 import * as Server from "@effect/rpc/Server"
@@ -5,46 +7,76 @@ import * as RpcSchema from "@effect/rpc/Schema"
 import * as Schema from "@effect/schema/Schema"
 import { FetchDataSource } from "@effect/rpc/DataSource"
 
+// Post schema
+const PostId = pipe(
+  Schema.number,
+  Schema.positive(),
+  Schema.int(),
+  Schema.brand("PostId"),
+)
+type PostId = Schema.To<typeof PostId>
+
+const Post = Schema.struct({
+  id: PostId,
+  body: Schema.string,
+})
+const CreatePost = pipe(Post, Schema.omit("id"))
+
+// Post service schema
 const posts = RpcSchema.make({
   create: {
-    output: Schema.any,
+    input: CreatePost,
+    output: Post,
+  },
+  list: {
+    output: Schema.chunk(Post),
   },
 })
 
+// Post service router
 const postsRouter = Server.router(posts, {
-  create: Effect.succeed({}),
+  create: (post) =>
+    Effect.succeed({
+      id: PostId(1),
+      ...post,
+    }),
+
+  list: Effect.succeed(
+    Chunk.fromIterable([
+      {
+        id: PostId(1),
+        body: "Hello world!",
+      },
+    ]),
+  ),
 })
 
+// Root service schema
 const schema = RpcSchema.make({
+  // Add nested post service
+  posts,
+
   greet: {
     input: Schema.string,
     output: Schema.string,
-    error: Schema.number,
   },
 
   currentTime: {
     output: Schema.dateFromString,
   },
-
-  posts,
 })
 
+// Root service router
 const router = Server.router(schema, {
   greet: (name) => Effect.succeed(`Hello ${name}!`),
   currentTime: Effect.sync(() => new Date()),
   posts: postsRouter,
 })
 
-const test = Server.handlerDirect(router)
+// TODO: Connect router to HTTP server
+export const handler = Server.handler(router)
 
-const result = Effect.runSync(
-  test({
-    method: "greet",
-    input: "John",
-  }),
-)
-console.log(result)
-
+// Create client
 const client = Client.make(
   schema,
   FetchDataSource.make({
@@ -52,6 +84,4 @@ const client = Client.make(
   }),
 )
 
-const r = Effect.runSync(router.undecoded.posts.create)
-
-client._unsafeDecode("posts.create", r)
+Effect.runPromise(client.posts.create({ body: "Hello!" }))
