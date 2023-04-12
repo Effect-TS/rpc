404b3eff333c5afbe845b6dcf0b0f59c8c457b42
Tim Smart <hello@timsmart.co>
2023-04-13 09:41:37 +1200

    refactor: clean up Server module

--- src/internal/server.ts
+++ src/internal/server.ts
@@ -1,14 +1,26 @@
+import * as Chunk from "@effect/data/Chunk"
 import * as Either from "@effect/data/Either"
 import { identity, pipe } from "@effect/data/Function"
 import * as Effect from "@effect/io/Effect"
+import * as Query from "@effect/query/Query"
 import type { RpcRequest } from "@effect/rpc/DataSource"
 import type { RpcNotFound } from "@effect/rpc/Error"
-import type { RpcHandler, RpcHandlers, RpcRouter } from "@effect/rpc/Server"
-import * as dataSource from "@effect/rpc/internal/dataSource"
+import type {
+  RpcRequestSchema,
+  RpcSchema,
+  RpcService,
+} from "@effect/rpc/Schema"
+import type {
+  RpcHandler,
+  RpcHandlers,
+  RpcRouter,
+  RpcUndecodedClient,
+} from "@effect/rpc/Server"
+import * as codec from "@effect/rpc/internal/codec"
 import { decode, encode } from "@effect/rpc/internal/codec"
+import * as dataSource from "@effect/rpc/internal/dataSource"
 import { methodsMap } from "@effect/rpc/internal/schema"
 import * as Schema from "@effect/schema/Schema"
-import type { RpcRequestSchema } from "@effect/rpc/Schema"
 
 /** @internal */
 export const schemaHandlersMap = <H extends RpcHandlers>(
@@ -109,3 +121,88 @@ export const handleRequestUnion = <R extends RpcRouter.Base>(router: R) => {
     return (handler as any)((request as RpcRequest).input) as any
   }
 }
+
+/** @internal */
+export const router = <
+  S extends RpcService.DefinitionWithId,
+  H extends RpcHandlers.FromService<S>,
+>(
+  schema: S,
+  handlers: H,
+): RpcRouter<S, H> => ({
+  schema,
+  handlers,
+  undecoded: makeUndecodedClient(schema, handlers),
+})
+
+/** @internal */
+export const handler = <R extends RpcRouter.Base>(
+  router: R,
+): ((
+  u: unknown,
+) => Effect.Effect<RpcHandlers.Services<R["handlers"]>, never, unknown>) => {
+  const handler = handleSingleRequest(router)
+
+  return (u) =>
+    pipe(
+      codec.requestDecoder(u),
+      Effect.orDie,
+      Effect.flatMap((requests) => Effect.collectAllPar(requests.map(handler))),
+      Effect.map(Chunk.toReadonlyArray),
+    )
+}
+
+/** @internal */
+export const handlerRaw: <R extends RpcRouter.Base>(
+  router: R,
+) => <Req extends RpcRequestSchema.To<R["schema"]>>(
+  request: Req,
+) => Req extends { _tag: infer M }
+  ? RpcHandler.FromMethod<M, R["handlers"]>
+  : never = handleRequestUnion as any
+
+/** @internal */
+export const makeUndecodedClient = <
+  S extends RpcService.DefinitionWithId,
+  H extends RpcHandlers.FromService<S>,
+>(
+  schemas: S,
+  handlers: H,
+): RpcUndecodedClient<H> =>
+  Object.entries(handlers as RpcHandlers).reduce(
+    (acc, [method, definition]) => {
+      if ("handlers" in definition) {
+        return {
+          ...acc,
+          [method]: makeUndecodedClient(
+            schemas[method] as any,
+            definition.handlers as any,
+          ),
+        }
+      }
+
+      const schema = schemas[method] as RpcSchema.Any
+
+      if (Effect.isEffect(definition)) {
+        return {
+          ...acc,
+          [method]: pipe(
+            definition,
+            Effect.flatMap(codec.encodeEffect(schema.output)),
+            Query.fromEffect,
+          ),
+        }
+      }
+
+      return {
+        ...acc,
+        [method]: (input: unknown) =>
+          pipe(
+            (definition as RpcHandler.IO<any, any, any, any>)(input),
+            Effect.flatMap(codec.encodeEffect(schema.output)),
+            Query.fromEffect,
+          ),
+      }
+    },
+    {} as any,
+  )
