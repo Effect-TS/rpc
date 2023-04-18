423af4580751b6f3a2c462cb45efc34ffde3fd5b
Tim Smart <hello@timsmart.co>
2023-04-19 09:31:23 +1200

    chore: monorepo

--- packages/rpc/vitest.config.ts
+++ packages/rpc/vitest.config.ts
@@ -1,23 +1,22 @@
 /// <reference types="vitest" />
-
 import babel from "@vitejs/plugin-react"
 import path from "path"
 import { defineConfig } from "vite"
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const babelConfig = require("./.babel.mjs.json")
+const babelConfig = require("../../.babel.mjs.json")
 
 export default defineConfig({
   plugins: [babel({ babel: babelConfig })],
   test: {
-    include: ["packages/*/test/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    exclude: ["packages/*/test/utils/**/*.ts", "./test/**/*.init.ts"],
+    include: ["./test/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    exclude: ["./test/utils/**/*.ts", "./test/**/*.init.ts"],
     globals: true,
   },
   resolve: {
     alias: {
-      "@effect/rpc/test": path.join(__dirname, "packages/rpc/test"),
-      "@effect/rpc": path.join(__dirname, "packages/rpc/src"),
+      "@effect/rpc/test": path.join(__dirname, "test"),
+      "@effect/rpc": path.join(__dirname, "src"),
     },
   },
 })
