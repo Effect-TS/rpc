4b3205756cf12612211fcf88f5aa2672431e3ee3
Tim <hello@timsmart.co>
2023-04-19 13:31:43 +1200

    feat: add http transport (#16)

---
+++ packages/http/vitest.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest" />
+import babel from "@vitejs/plugin-react"
+import path from "path"
+import { defineConfig } from "vite"
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const babelConfig = require("../../.babel.mjs.json")
+
+export default defineConfig({
+  plugins: [babel({ babel: babelConfig })],
+  test: {
+    include: ["./test/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    exclude: ["./test/utils/**/*.ts", "./test/**/*.init.ts"],
+    globals: true,
+  },
+  resolve: {
+    alias: {
+      "@effect/rpc-http/test": path.join(__dirname, "test"),
+      "@effect/rpc-http": path.join(__dirname, "src"),
+    },
+  },
+})
