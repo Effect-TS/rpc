55ba56eb09c6066126af5f224652a4c5d413f8d4
Alex Dixon <alex.dixon@ei.tech>
2023-08-02 19:10:45 -0700

    upgrade deps (#78)
    Co-authored-by: Tim <hello@timsmart.co>

---
+++ .changeset/thin-phones-live.md
@@ -0,0 +1,9 @@
+---
+"@effect/rpc-http-node": patch
+"@effect/rpc-http": patch
+"@effect/rpc-nextjs": patch
+"@effect/rpc": patch
+"@effect/rpc-webworkers": patch
+---
+
+upgrade deps
--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,29 +4,30 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: "module"
   },
   settings: {
     "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"],
+      "@typescript-eslint/parser": [".ts", ".tsx"]
     },
     "import/resolver": {
       typescript: {
-        alwaysTryTypes: true,
-      },
-    },
+        alwaysTryTypes: true
+      }
+    }
   },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@effect/eslint-plugin/recommended"
   ],
   plugins: [
     "deprecation",
     "import",
     "sort-destructure-keys",
     "simple-import-sort",
-    "codegen",
+    "codegen"
   ],
   rules: {
     "codegen/codegen": "error",
@@ -49,7 +50,7 @@ module.exports = {
     "deprecation/deprecation": "off",
     "@typescript-eslint/array-type": [
       "warn",
-      { default: "generic", readonly: "generic" },
+      { default: "generic", readonly: "generic" }
     ],
     "@typescript-eslint/member-delimiter-style": 0,
     "@typescript-eslint/no-non-null-assertion": "off",
@@ -61,8 +62,8 @@ module.exports = {
       "error",
       {
         argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-      },
+        varsIgnorePattern: "^_"
+      }
     ],
     "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/camelcase": "off",
@@ -72,5 +73,19 @@ module.exports = {
     "@typescript-eslint/no-array-constructor": "off",
     "@typescript-eslint/no-use-before-define": "off",
     "@typescript-eslint/no-namespace": "off",
-  },
+    "@effect/dprint": [
+      "error",
+      {
+        config: {
+          indentWidth: 2,
+          lineWidth: 120,
+          semiColons: "asi",
+          quoteStyle: "alwaysDouble",
+          trailingCommas: "never",
+          operatorPosition: "maintain",
+          "arrowFunction.useParentheses": "force"
+        }
+      }
+    ]
+  }
 }
